2d500cbe5b11f4c069cd35cfcfe1dc3f1f00cc72
MedAnisse <Med-anisse.belhadj@outlook.com>
2021-06-27 22:49:34 +0200

    base/easyadmin recup

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -1,26 +1,32 @@
 <?php
 
 namespace App\Controller\Admin;
-
+use App\Entity\Bulletin;
 use App\Entity\Comptable;
+use App\Entity\Directeur;
+use App\Entity\DirecteurGeneral;
+use App\Entity\Employe;
+use App\Entity\Filiale;
+use App\Entity\Poste;
+use App\Entity\Presence;
+use App\Entity\Rh;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Contracts\Controller\DashboardControllerInterface;
-use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 
 class DashboardController extends AbstractDashboardController
 {
-    #[Route('/admin', name: 'admin')]
+    /**
+     * @Route("/admin", name="admin")
+     */
     public function index(): Response
     {
         return parent::index();
-        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-       // return $this->render('@EasyAdmin/page/content.html.twig');
     }
+
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
@@ -29,7 +35,18 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linkToCrud('The Label', 'fas fa-list', Comptable::class);
+        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linkToCrud('Bulletin', 'fas fa-list', Bulletin::class);
+        yield MenuItem::linkToCrud('Comptable', 'fas fa-list', Comptable::class);
+        yield MenuItem::linkToCrud('DirecteurGeneral', 'fas fa-list', DirecteurGeneral::class);
+        yield MenuItem::linkToCrud('Employe', 'fas fa-list', Employe::class);
+        yield MenuItem::linkToCrud('Filiale', 'fas fa-list', Filiale::class);
+        yield MenuItem::linkToCrud('Poste', 'fas fa-list', Poste::class);
+        yield MenuItem::linkToCrud('Presence', 'fas fa-list', Presence::class);
+        yield MenuItem::linkToCrud('Rh', 'fas fa-list', Rh::class);
+        yield MenuItem::linkToCrud('Directeur', 'fas fa-list', Directeur::class);
+        yield MenuItem::linkToCrud('User', 'fas fa-list', User::class);
+        
+
     }
 }
--- src/Controller/Admin/EmployeCrudController.php
+++ src/Controller/Admin/EmployeCrudController.php
@@ -2,22 +2,22 @@
 
 namespace App\Controller\Admin;
 
-use App\Entity\Comptable;
-use App\Entity\Directeur;
+use App\Entity\Employe;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
-
-class ComptableCrudController extends AbstractCrudController
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+class EmployeCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
     {
-        return Comptable::class;
+        return Employe::class;
     }
 
 
@@ -42,11 +42,12 @@ class ComptableCrudController extends AbstractCrudController
             ArrayField::new('roles')
         ];
     }
-    public function configureActions(Actions $actions): Actions
+
+   public function configureActions(Actions $actions): Actions
     {
         $sendInvoice = Action::new('sendInvoice', 'Confirmer', 'fa fa-envelope')
-            ->linkToUrl(function (Comptable $comptable) {
-                return '/afficherEmploye/'.$comptable->getId();
+            ->linkToUrl(function (Employe $employe) {
+               return '/afficherEmploye/'.$employe->getId();
             });
         return $actions
             ->add(Crud::PAGE_INDEX, $sendInvoice->addCssClass('btn btn-outline-info mb-2'));
